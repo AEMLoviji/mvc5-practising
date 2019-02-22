badc148a8da723a32f2a8481c66bd45d0a83e0c6
AEMLoviji <AEMLoviji@gmail.com>
2019-02-23 02:11:38 +0300

    Added list and details actions

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
-﻿using SchoolProject.Models;
+﻿using System.Linq;
 using System.Web.Mvc;
+using SchoolProject.Models;
 
 namespace SchoolProject.Controllers
 {
     public class HomeController : Controller
     {
         private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Students.ToList());
         }
+
+        public ActionResult Details(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
 
         public ActionResult About()
         {
@@ -23,6 +36,12 @@ namespace SchoolProject.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
